042985b3a659a040b39c84507f43b2a7eef2c5a0
BrianLane <Brianlane2@gmail.com>
2025-04-01 14:13:40 -0700

    Security: Remove hardcoded Devise secrets and use Rails credentials instead

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,7 +14,9 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  # config.secret_key = 'DEVISE_SECRET_KEY_REDACTED'
+  # 
+  # Use Rails credentials for the secret key, or fall back to secret_key_base
+  # config.secret_key = Rails.application.credentials.devise&.secret_key || Rails.application.secret_key_base
 
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
@@ -126,7 +128,9 @@ Devise.setup do |config|
   config.stretches = Rails.env.test? ? 1 : 12
 
   # Set up a pepper to generate the hashed password.
-  # config.pepper = 'DEVISE_PEPPER_REDACTED'
+  # 
+  # Use Rails credentials for the pepper, or don't set it to rely on secret_key_base
+  # config.pepper = Rails.application.credentials.devise&.pepper
 
   # Send a notification to the original email when the user's email is changed.
   # config.send_email_changed_notification = false
