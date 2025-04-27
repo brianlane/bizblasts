3e4088b874c857810e649b54588c461a18c8046a
BrianLane <Brianlane2@gmail.com>
2025-04-27 10:05:07 -0700

    update-buildjs

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,7 @@
 // Application JavaScript
 // Your custom JavaScript can be added here
+
+import "./modules/availability_manager";
+import "./modules/booking_form_helper";
+
+// Add any other necessary imports
