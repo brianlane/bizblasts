89cbc38443853b3b88d3085ccc2c84b144e9f0d2
BrianLane <Brianlane2@gmail.com>
2025-04-27 15:04:16 -0700

    removeVerification

---
+++ spec/services/calendar_sync_service_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe CalendarSyncService, type: :service do
+  describe '#sync_booking' do
+    it 'should sync a booking with the calendar service' do
+      # TODO: Implement actual test logic here
+      pending 'Implementation pending'
+    end
+  end
+end 
