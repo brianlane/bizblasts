9a02b27bbb25b30081c1c28383f09b54d082cbf6
BrianLane <Brianlane2@gmail.com>
2025-05-06 15:11:41 -0700

    Fix migration order issues by renaming and updating migration files

--- db/migrate/20250506215552_add_primary_and_position_to_active_storage_attachments.rb
+++ db/migrate/20250506215552_add_primary_and_position_to_active_storage_attachments.rb
@@ -1,5 +1,8 @@
 class AddPrimaryAndPositionToActiveStorageAttachments < ActiveRecord::Migration[8.0]
   def change
+    # Skip if the table doesn't exist yet (it will be created with these columns by the ActiveStorage migration)
+    return unless table_exists?(:active_storage_attachments)
+    
     unless column_exists?(:active_storage_attachments, :primary)
       add_column :active_storage_attachments, :primary, :boolean
     end
