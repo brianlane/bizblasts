05a750e7edd11ec2359afa3f7129afb2a6dc166d
BrianLane <Brianlane2@gmail.com>
2025-05-14 12:59:28 -0700

    update-manifest-2

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,24 +1,9 @@
+// This is the Sprockets asset manifest for BizBlasts
+// It includes only unique, essential assets to avoid duplicate digest stamping
+
 //= link_tree ../images
 //= link_directory ../javascripts .js
-// = link_directory ../stylesheets .css // Removed to let Propshaft handle main CSS
-//= link application.js
-//= link controllers/application.js
-//= link controllers/hello_controller.js
-//= link controllers/index.js
-//= link_tree ../../javascript .js
-//= link_tree ../builds
-//= link active_admin.js
-//= link active_admin.css
-//= link ../builds/active_admin.css
-//= link application.css
-//= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
-
-// Include any vendor JavaScript libraries
-//= link_tree ../../../vendor/javascript .js
-
 // Precompile ActiveAdmin JavaScript
 //= link active_admin.js
-
-// Include all compiled CSS assets (Propshaft output)
-//= link_tree ../builds
+// Include all compiled CSS and JS from builds (Propshaft output)
+//= link_tree ../builds 
