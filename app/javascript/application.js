53edee646f372674a8e6030a5551cb6398eea061
BrianLane <Brianlane2@gmail.com>
2025-06-02 11:35:39 -0700

    javascript-tiles-30-industry-2

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,12 +5,5 @@ import "./modules/availability_manager";
 import "./modules/booking_form_helper";
 import "./modules/customer_form_helper";
 import "./modules/policy_acceptance";
-import "./modules/category_showcase"; // For category showcase feature
+import "./modules/category_showcase"; 
 import "./cart";
-
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-// import "@hotwired/turbo-rails"
-// import "controllers"
-
-// The following line for test_module is commented out as it's an example.
-// import "./modules/test_module";
