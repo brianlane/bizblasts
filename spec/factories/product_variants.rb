fea337911a18b309aaf6b4c2485662774aaaa19c
BrianLane <Brianlane2@gmail.com>
2025-06-07 12:09:30 -0700

    fix-debug-output-scientific-notation

--- spec/factories/product_variants.rb
+++ spec/factories/product_variants.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :product_variant do
     association :product
     name { ["Small", "Medium", "Large", "Red", "Blue", "Green"].sample + " #{SecureRandom.hex(2)}" }
-    price_modifier { [nil, -5.0, 0, 10.0, 2.5].sample }
+    price_modifier { [nil, -5.00, 0.00, 10.00, 2.50].sample }
     stock_quantity { 100 }
 
     # Ensure business consistency via product
