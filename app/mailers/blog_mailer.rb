7103868a48ab9b6258b008f3c4441c415b225254
BrianLane <Brianlane2@gmail.com>
2025-07-11 08:04:32 -0700

    update-blog-mailer

--- app/mailers/blog_mailer.rb
+++ app/mailers/blog_mailer.rb
@@ -5,6 +5,8 @@ class BlogMailer < ApplicationMailer
   def new_post_notification(user, blog_post)
     @user = user
     @blog_post = blog_post
+    # For manager or staff recipients, capture their business to build tenant-specific URLs in templates
+    @business = user.business if user.manager? || user.staff?
     
     mail(
       to: user.email,
@@ -18,6 +20,8 @@ class BlogMailer < ApplicationMailer
     @blog_posts = blog_posts
     @week_start = 1.week.ago.beginning_of_week
     @week_end = Date.current.end_of_week
+    # Provide business context for manager or staff recipients so views can generate correct subdomain links
+    @business = user.business if user.manager? || user.staff?
     
     mail(
       to: user.email,
