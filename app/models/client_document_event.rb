0205f04fe5d6e7d8564aa5e8ffe1ff3e662d26d9
BrianLane <Brianlane2@gmail.com>
2026-01-05 08:14:15 -0700

    fix-500

--- app/models/client_document_event.rb
+++ app/models/client_document_event.rb
@@ -10,6 +10,25 @@ class ClientDocumentEvent < ApplicationRecord
 
   before_validation :set_business_from_document
 
+  def self.ransackable_attributes(auth_object = nil)
+    %w[
+      id
+      business_id
+      client_document_id
+      event_type
+      message
+      actor_type
+      actor_id
+      ip_address
+      created_at
+      updated_at
+    ]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    %w[business client_document]
+  end
+
   private
 
   def set_business_from_document
